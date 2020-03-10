997955ec17a9fad3ee33194bd231b939e392238a
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-03-11 00:45:29 +1100

    Update connection_test.rb
    Fixed a bug in this that was causing a wrong number of arguments (given 1, expected 0) (ArgumentError)

--- connection_test.rb
+++ connection_test.rb
@@ -1,16 +1,14 @@
-def ssh_test(command)
+def test_connection(command)
   Net::SSH.start($ip_address, $user_name, :password => $password) do |ssh|  
     $result = ssh.exec!command
   end
+if $result.gsub(/[^0-9A-Za-z]/, '') == $user_name
+    $connect_test_result = "Connection Succesful"
+    puts "Status: Connection with UnRaid server Succesful"
+else
+    puts "Connection Failed, please check your settings..."
+end
+return_on_enter
 end
 
-def test_connection() 
-  if $result.gsub(/[^0-9A-Za-z]/, '') == $user_name
-      $connect_test_result = "Connection Succesful"
-      puts "Status: Connection with UnRaid server Succesful"
-  else
-      puts "Connection Failed, please check your settings..."
-  end
-  return_on_enter($current_menu)
-end
-# Confirms connection by checking the directory eg. if logged in as root: the default directory should be root/
+# Confirms connection by checking the directory (pwd) that you log in as (users normally start in their home directory eg. Login in as root the default directory should be root/
