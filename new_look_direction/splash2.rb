d921b538f2b7bbcda928609394ad0f5be4b01ece
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-03-12 07:53:03 +1100

    New Menu Look Plan
    This is the direction I want to take the menu system, have designed the loolk all myself (apart from the ascii computer. looks awesome with that lolcat, but i can't get lolcat to not print its name in top corner (not that i've tried that hard) :)

---
+++ new_look_direction/splash2.rb
@@ -0,0 +1,100 @@
+
+      ooooooooooooooooooooooooooooooooooooo                        ██    ██ ███    ██ ██████   █████  ██ ██████    ███████ ███████ ██   ██  v0.6       
+      8                                .d88                        ██    ██ ████   ██ ██   ██ ██   ██ ██ ██   ██   ██      ██      ██   ██        
+      8  oooooooooooooooooooooooooooood8888                        ██    ██ ██ ██  ██ ██████  ███████ ██ ██   ██   ███████ ███████ ███████        
+      8  8888888888888888888888888P"   8888    oooooooooooooooo    ██    ██ ██  ██ ██ ██   ██ ██   ██ ██ ██   ██        ██      ██ ██   ██        
+      8  8888888888888888888888P"      8888    8              8     ██████  ██   ████ ██   ██ ██   ██ ██ ██████    ███████ ███████ ██   ██        
+      8  8888888888888888888P"         8888    8             d8     ______  _______ _______ _     _ ______   _____  _______  ______ ______        
+      8  8888888888888888P"            8888    8            d88     |     \ |_____| |______ |_____| |_____] |     | |_____| |_____/ |     \       
+      8  8888888888888P"               8888    8           d888     |_____/ |     | ______| |     | |_____] |_____| |     | |    \_ |_____/       
+      8  8888888888P"                  8888    8          d8888                                                                                  
+      8  8888888P"                     8888    8         d88888               ┌─────────┬─────────┬─────────┬─────────┬─────────┐                                                                        
+      8  8888P"                        8888    8        d888888               │  setup  │  tools  │ system  │  about  │   exit  │                                                          
+      8  8888oooooooooooooooooooooocgmm8888    8       d8888888               └─────────┴─────────┴─────────┴─────────┴─────────┘                                                               
+      8 .od88888888888888888888888888888888    8      d88888888                                                                                                 
+      8888888888888888888888888888888888888    8     d888888888                                                                                                
+                                               8    d8888888888                                                                 
+         ooooooooooooooooooooooooooooooo       8   d88888888888                                                       
+        d                       ...oood8b      8  d888888888888                                                        
+       d              ...oood888888888888b     8 d8888888888888                                                                             
+      d     ...oood88888888888888888888888b    8d88888888888888                                                                     
+     dood8888888888888888888888888888888888b                         
+
+                                                                                               
+                                                                                               
+                                                                                                    
+                                                                                                                              
+                                                                                                          
+                                           ┌────┴─────────────────────────────┐                                                                                              
+                                           │    Version 0.6  - March 2020     │                                                               
+     ┌──────────────────────────────────┐  ├──────────────────────────────────┤                                                                                                    
+     │ This was built and intend for    │  │  Menu system was a last minute   │                                                                                                          
+     │ people who are new to unraid and │  │  change and was designed by me.  │                                                                                                                    
+     │ Want to learn abit about what    ├──┤  I really like the simplicity    │   
+     │ they can do in the command line. │  │       of it, no bullshit.        │                                                                    
+     │ I have alot of plans for this,   │  │                                  │                                                          
+     │ and I always welcome bug reports │  │       |c|h|r|i|s|R|3|2|          │
+     │ and any recommendations or help  │  └──────────────────────────────────┘
+     │ with the project.                │         
+     └──────────────────────────────────┘    
+
+┌────────────────┴─────────────────┐
+│           Version 0.6            │  
+├──────────────────────────────────┤
+│ This was built and intend for    │
+│ people who are new to unraid and │
+│ Want to learn abit about what    │
+│ they can do in the command line. │
+│ I have alot of plans for this,   │
+│ and I always welcome bug reports │
+│ and any recommendations or help  │
+│ with the project.                │
+│                                  │
+│  Menu system was a last minute   │
+│  change and was designed by me.  │
+│  I really like the simplicity    │
+│       of it, no bullshit.        │
+│                                  │
+│   Find me on GitHub @ chrisR32   │
+│                                  │
+│ Art & Font: https://ascii.co.uk  │
+│                                  │ 
+│       |c|h|r|i|s|R|3|2|          │     
+└──────────────────────────────────┘
+
+
+
+┌──────────────────────────────────┬─────────┬────────────────────────┬────────────────┐
+│               Col1               │  Col2   │          Col3          │ Numeric Column │
+├──────────────────────────────────┼─────────┼────────────────────────┼────────────────┤
+│ Value 1                          │ Value 2 │ 123                    │           10.0 │
+│ Separate                         │ cols    │ with a tab or 4 spaces │       -2,027.1 │
+│ This is a row with only one cell │         │                        │                │
+└──────────────────────────────────┴─────────┴────────────────────────┴────────────────┘
+
+├─ IP address                                                                    
+│                                                              
+├─ Login                                                                                 
+│                                                              
+├─ Password                                                               
+│                                                                                     
+└─ Test       
+
+├─ Hard Drives                                                                    
+│                                                              
+├─ Drive Shares                                                                                 
+│                                                              
+├─ Docker                                                               
+│                                                                                     
+└─ General 
+       
+         ├─ Hard Drives                                                                    
+    CPU ─┤                                                                        
+         ├─ General                                                                                 
+ Memory ─┤                                                                        
+         ├─ Misc.                                                              
+Network ─┘        
+
+├─ Yes                                     
+│                                       
+└─ No  
